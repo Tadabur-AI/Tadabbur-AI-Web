f7c149560427da16ed3c4cc2c04a9ff818c0e1fe
Sharjeel Baig <dr.sharjeel.6@gmail.com>
2025-10-29 02:30:35 +0500

    refactor: comment out unused imports in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/tailwind.css'
 import './css/root.css'
 import { Router } from './routing/router'
-import DownForMaintanance from './components/common/DownForMaintanance'
+// import DownForMaintanance from './components/common/DownForMaintanance'
 
 createRoot(document.getElementById('root')!).render(
   <>
